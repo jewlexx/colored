3e86c6415c564716c3a4d6db1e66697f90c21672
Juliette Cordor <grassojames5@gmail.com>
2022-07-28 19:19:23 +1000

    added back tests that require println statements

---
+++ tests/printing.rs
@@ -0,0 +1,62 @@
+#![cfg(not(feature = "no-color"))]
+#![allow(unused_imports)]
+
+extern crate colored;
+
+use colored::*;
+
+mod lib_tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let toto = "toto";
+        println!("{}", toto.red());
+        println!("{}", String::from(toto).red());
+        println!("{}", toto.blue());
+
+        println!("blue style ****");
+        println!("{}", toto.bold());
+        println!("{}", "yeah ! Red bold !".red().bold());
+        println!("{}", "yeah ! Yellow bold !".bold().yellow());
+        println!("{}", toto.bold().blue());
+        println!("{}", toto.blue().bold());
+        println!("{}", toto.blue().bold().underline());
+        println!("{}", toto.blue().italic());
+        println!("******");
+        println!("test clearing");
+        println!("{}", "red cleared".red().clear());
+        println!("{}", "bold cyan cleared".bold().cyan().clear());
+        println!("******");
+        println!("Bg tests");
+        println!("{}", toto.green().on_blue());
+        println!("{}", toto.on_magenta().yellow());
+        println!("{}", toto.purple().on_yellow());
+        println!("{}", toto.magenta().on_white());
+        println!("{}", toto.cyan().on_green());
+        println!("{}", toto.black().on_white());
+        println!("******");
+        println!("{}", toto.green());
+        println!("{}", toto.yellow());
+        println!("{}", toto.purple());
+        println!("{}", toto.magenta());
+        println!("{}", toto.cyan());
+        println!("{}", toto.white());
+        println!("{}", toto.white().red().blue().green());
+        println!("{}", toto.truecolor(255, 0, 0));
+        println!("{}", toto.truecolor(255, 255, 0));
+        println!("{}", toto.on_truecolor(0, 80, 80));
+        // uncomment to see term output
+        // assert!(false)
+    }
+}
+
+mod customcolor_tests {
+    use super::*;
+
+    #[test]
+    fn main() {
+        let my_color = CustomColor::new(0, 120, 120);
+        println!("{}", "Greetings from Ukraine".custom_color(my_color));
+    }
+}
